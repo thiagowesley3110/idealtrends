004e895f61c097b7dc4291c54f67f916b09b0204
thiagowesley3110 <thiagosurff1010@yahoo.com.br>
2024-02-23 19:01:35 -0300

    Teste para empresa

---
+++ main.js
@@ -0,0 +1,17 @@
+
+  const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable:true
+    },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+            
+
+        
+    },    
+});
